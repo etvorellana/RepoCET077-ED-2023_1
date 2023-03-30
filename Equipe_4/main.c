6dcfa92847ec24f831db754d7e5bf16e6c5bea5d
GustavoCosta <109289198+ProgGusta@users.noreply.github.com>
2023-03-30 16:31:55 -0300

    Update main.c

--- Equipe_4/main.c
+++ Equipe_4/main.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -8,45 +7,47 @@
 int main(void) {
 
   TSongs *acervo;
-  int id = 1, tam = 0;
-  int total, t = 100;
+  int tam = 0;
 
   acervo = carregaDados("Data/newSongs.csv", &tam);
 
   // Atividada para o dia 27/03
   // INCLUSAO
-  int newtam = tam / 3, tamaux = 0; // defino o tamanho para 1/3 do acervo
-  TSongs *newacervo = (TSongs *)malloc(newtam * sizeof(TSongs)); // nova lista
+  int newtam = tam / 3, tamaux = 0; 
+  TSongs *newacervo = (TSongs *)malloc(newtam * sizeof(TSongs)); 
   int acertos = 0, tentativas = 0;
 
   while (tamaux < newtam) {
     int NA = rand() % 20000;
-    //printf("%d\n\n", tamaux);
     if (NA >= 0 && NA < tam) {
       tentativas++;
-      //printf("Tentativa: %d\n\n", tentativas);
+      printf("Tentativa: %d | Numero: %d\n\n", tentativas, NA);
       if (IncRegistro(acervo[NA], newacervo, &tamaux)) {
-        //printf("Sucesso: %d\n\n", acertos);
+        //printf("Sucesso inclusao: %d\n\n", acertos);
         acertos++;
       }
     }
   }
   printf("Na inclusão\nForam %d Tentativas!\nForam %d Acertos!\n", tentativas, acertos);
 
-// ainda nao foi implementado o sistema de remoçao
+// REMOCAO
   acertos = 0, tentativas = 0;
-  while (0){
+  while (acertos != newtam){
     int NA = rand() % 20000;
     if (NA >= 0 && NA < tam) {
       tentativas++;
       //printf("Tentativa: %d\n\n", tentativas);
-      if (RemRegistro(acervo[NA], newacervo, &tamaux)) {
-        //printf("Sucesso: %d\n\n", acertos);
+      if (RemRegistro(newacervo[NA], newacervo, &tamaux)) {
+        //printf("Sucesso remocão: %d\n\n", acertos);
         acertos++;
       }
     }
   }
   printf("Na exclusão\nForam %d Tentativas!\nForam %d Acertos!", tentativas, acertos);
+  //-----------------------------------------------------------
+  // Atividade para o dia 03/04
+  // busca binaria?
+  //-----------------------------------------------------------
   limpaAcervo(acervo, tam);
   limpaAcervo(newacervo, newtam);
 }
