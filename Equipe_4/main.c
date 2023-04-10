ec3d0e52d36b2a1b0392961844681b2a92715601
GustavoCosta <109289198+ProgGusta@users.noreply.github.com>
2023-04-10 13:13:04 -0300

    Update main.c

--- Equipe_4/main.c
+++ Equipe_4/main.c
@@ -5,61 +5,50 @@
 #include "bidefun.h"
 
 int main(void) {
+  printf("Ola, vamos criar sua lista, aguarde\n...\n");
 
   TSongs *acervo;
   int tam = 0;
 
   acervo = carregaDados("Data/newSongs.csv", &tam);
-
-  // Atividada para o dia 27/03
-  // INCLUSAO
-  int newtam = tam / 3, tamaux = 0; 
-  TSongs *newacervo = (TSongs *)malloc(newtam * sizeof(TSongs)); 
-  int existe, acertos = 0, tentativas = 0;
-  int resp = buscaBinporPOSICAO(14000, acervo, tam, &existe);
-  printf("BUSCA: %d", resp);
-   printf("\ntamanho original = %d\n",tam);
-  if(RemRegistro_nao_ordenado(acervo[tam - 5], acervo , &tam)){
-       printf("\nRemoção Feita\nNOVO TAMANHO => %d\n",tam);
-  }
-  if(RemRegistro_ordenado(acervo[tam - 4], acervo , &tam)){
-       printf("\nRemoção Feita\nNOVO TAMANHO => %d\n",tam);
-  }
-  
-/*
-  while (tamaux < newtam) {
-    int NA = rand() % 20000;
-    if (NA >= 0 && NA < tam) {
-      tentativas++;
-      //printf("Tentativa: %d | Numero: %d\n\n", tentativas, NA);
-      if (IncRegistro(acervo[NA], newacervo, &tamaux)) {
-        printf("Sucesso inclusao: %d\n\n", acertos);
-        acertos++;
-      }
-    }
-  }
-  printf("Na inclusão\nForam %d Tentativas!\nForam %d Acertos!\n", tentativas, acertos);
-
-
-// REMOCAO
-  acertos = 0, tentativas = 0;
-  while (acertos != newtam){
-    int NA = rand() % 20000;
-    if (NA >= 0 && NA < tam) {
-      tentativas++;
-      //printf("Tentativa: %d\n\n", tentativas);
-      if (RemRegistro_nao_ordenado(newacervo[NA], newacervo, &tamaux)) {
-        //printf("Sucesso remocão: %d\n\n", acertos);
-        acertos++;
-      }
-    }
-  }
-  printf("Na exclusão\nForam %d Tentativas!\nForam %d Acertos!", tentativas, acertos);
-*/
   //-----------------------------------------------------------
-  // Atividade para o dia 03/04
-  // busca binaria?
+  // Atividade para o dia 10/04
+  // cria a lista
+  TListaLinear *lista = criaListaLinear(50, 1);
+  printf("--- Lista Criada ---\n...\n");
+  // insere na lista
+  int cont = 0;
+  while (cont < 50) {
+    int NA = rand() % 1000;
+    insereNaLista(acervo[NA], lista);
+    cont++;
+  }
+  printf("--- Lista Populada ---\n...\n");
+  // busca
+  int pos = 0;
+  while (1) {
+    printf("\nInsira o ranking da musica que deseja buscar da lista(-1 para "
+           "encerrar a busca)\n");
+    scanf("%d", &pos);
+    if (pos == -1) {
+      break;
+    }
+    buscaNaLista(acervo[pos], lista);
+  }
+  // remove na lista
+  cont = 0;
+  while (cont < 50) {
+    int NA = rand() % 1000;
+    removeDaLista(acervo[NA], lista);
+
+    cont++;
+  }
+  printf("--- Lista Esvaziada ---");
+
+  //
   //-----------------------------------------------------------
   limpaAcervo(acervo, tam);
-  limpaAcervo(newacervo, newtam);
+  // limpaAcervo(newacervo, newtam);
+
+  return 0;
 }
