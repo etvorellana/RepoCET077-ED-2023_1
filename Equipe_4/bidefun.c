84041cb53837251c72e91958a39854db7fc2f940
GustavoCosta <109289198+ProgGusta@users.noreply.github.com>
2023-04-10 13:14:30 -0300

    Update bidefun.c

--- Equipe_4/bidefun.c
+++ Equipe_4/bidefun.c
@@ -124,7 +124,185 @@ int BuscaPorRank(int R, TSongs *lista, int tam) {
   return tam;
 }
 
-int IncRegistro(TSongs song, TSongs *lista, int *tamaux) {
+int busca_binaria(TSongs *lista, int tamanho, int item) {
+//printf("aaaaaaaaaaaaaaa%d", item);
+  int esquerda = 0, direita = tamanho - 1;
+  int meio = (esquerda + direita) / 2;
+  while (esquerda <= direita) {
+    //printf("entra no while");
+    if (lista[meio].Position == item) {
+printf("achamo\n");
+      return meio;
+    } else if (lista[meio].Position < item) {
+      esquerda = meio + 1;
+    } else {
+      direita = meio - 1;
+    }
+    meio = (esquerda + direita) / 2;
+  }
+printf("!achamo\n");
+
+  return esquerda;
+}
+
+int buscaBinporPOSICAO(int position, TSongs *lista, int tam, int *existe) {
+  // essa função sempre vai retornar um indice, o ponteiro existe
+  // que define se existe ou não.
+  //  << *existe = 0 que dizer que a chave buscada nao existe, e função vai
+  //  retorar onde ele deveria esta >>
+  //*existe = 1, a função vai retornar onde foi encontrado
+  int inicio = 0;                // inicio do vetor
+  int meio = (inicio + tam) / 2; // meio do vetor
+  *existe = 1;                   // diz se exies ou nao
+  while (inicio <= tam) {
+    if (position ==
+        lista[meio]
+            .Position) // verificação se o elemento procurado está no meio
+      return meio;
+    else {
+      if (position < lista[meio].Position) // verifica se o elemento do meio é
+                                           // maior do que o procurado
+        tam = meio - 1;
+      else // ou se é menor
+        inicio = meio + 1;
+    }
+    meio = (inicio + tam) / 2; // calculando o meio do vetor
+  }
+  *existe = 0;   // elemento não encontrado
+  return inicio; // elemento não encontrado e deveria esta nessa indice
+}
+
+int buscaBinporPOSICAOi(int position, TSongs *lista, int tam) {
+  int inicio = 0;                // inicio do vetor
+  int meio = (inicio + tam) / 2; // meio do vetor
+
+  while (inicio <= tam) {
+    if (position ==
+        lista[meio]
+            .Position) // verificação se o elemento procurado está no meio
+      return meio;
+    else {
+      if (position < lista[meio].Position) // verifica se o elemento do meio é
+                                           // maior do que o procurado
+        tam = meio - 1;
+      else // ou se é menor
+        inicio = meio + 1;
+    }
+    meio = (inicio + tam) / 2; // calculando o meio do vetor
+  }
+  return -1; // elemento não encontrado
+}
+
+int RemRegistro_ordenado(TSongs song, TSongs *lista, int *tamaux) {
+
+  int a = buscaBinporPOSICAOi(song.Position, lista, *tamaux);
+  if (a != -1) {
+
+    free(lista[a].ArtistName);
+    free(lista[a].SongName);
+    free(lista[a].PeakPositionXtimes);
+    lista[a] = lista[*tamaux - 1];
+    for (int i = a + 1; i < *tamaux; i++) {
+      lista[i] = lista[i + 1];
+    }
+
+    *tamaux = *tamaux - 1;
+    return 1;
+  }
+  return 0;
+}
+//
+TListaLinear *criaListaLinear(int cap, int eOrd) {
+  //printf("\nCriando lista\n");
+  TListaLinear *listaLinear = (TListaLinear *)malloc(sizeof(TListaLinear));
+
+  if (!listaLinear) {
+    printf("Erro de alocacao!!!");
+    exit(-1);
+  }
+  // printf("\nMorreu 0\n");
+  listaLinear->cap = cap;
+  listaLinear->tam = 0;
+  listaLinear->eOrd = eOrd;
+  // printf("\nMorreu 1\n");
+
+  if (eOrd) {
+    listaLinear->lista = (TSongs *)malloc(cap * sizeof(TSongs));
+    // printf("\nMorreu 2.1\n");
+  } else {
+    listaLinear->lista = (TSongs *)malloc((cap + 1) * sizeof(TSongs));
+    // printf("\nMorreu 2.2\n");
+  }
+
+  if (!listaLinear->lista) {
+    printf("Erro de alocacao!!!");
+    exit(-1);
+  }
+
+  //printf("\nRetornando lista\n");
+  return listaLinear;
+}
+//
+int buscaNaLista(TSongs song, TListaLinear *listaLinear) {
+//printf("entrou na busca\n");
+  int posicao;
+
+  if (listaLinear->tam == 0) {
+    return 0;
+  } else if (listaLinear->eOrd != 0) {
+//printf("entrou na busca ordenada\n");
+    posicao =
+        busca_binaria(listaLinear->lista, listaLinear->tam, song.Position);
+    //printf("\nEsta na lista!\n");
+    return posicao;
+  } else {
+//printf("entrou na busca !ordenada\n");
+    posicao = BuscaPorRank(song.Position, listaLinear->lista, listaLinear->tam);
+    //printf("\nNao esta na lista!\n");
+
+    return posicao;
+  }
+}
+
+// Foram retiradas as funções de busca das funções de inclusão, pois a busca é
+// feita no "insereNaLista"
+
+int IncRegistroOrd(TSongs song, TSongs *lista, int *tamaux, int pos) {
+  if (lista[pos].Position != song.Position) {
+    for (int i = *tamaux; i > pos; i--)
+      lista[i + 1] = lista[i]; // Abre espaço na lista para colocar o registro
+                               // na posição correta
+
+    lista[pos].Position = song.Position;
+    lista[pos].Key = song.Key;
+
+    // dava erro no tamanho estranhamente entao ao *2
+    // garante que vai ter o espaço
+    lista[pos].ArtistName = (char *)malloc(strlen(song.ArtistName) * 2);
+    strcpy(lista[pos].ArtistName, song.ArtistName);
+
+    lista[pos].SongName = (char *)malloc(strlen(song.SongName) * 2);
+    strcpy(lista[pos].SongName, song.SongName);
+
+    lista[pos].Days = song.Days;
+    lista[pos].Top10Times = song.Top10Times;
+    lista[pos].PeakPosition = song.PeakPosition;
+
+    lista[pos].PeakPositionXtimes =
+        (char *)malloc(strlen(song.PeakPositionXtimes) * 2);
+    strcpy(lista[pos].PeakPositionXtimes, song.PeakPositionXtimes);
+
+    lista[pos].PeakStreams = song.PeakStreams;
+    lista[pos].TotalStreams = song.TotalStreams;
+
+    *tamaux = *tamaux + 1;
+    return 1;
+  }
+
+  return 0;
+}
+
+int IncRegistro(TSongs song, TSongs *lista, int *tamaux, int pos) {
   if (*tamaux == 0) {
     lista[*tamaux].Position = song.Position;
     lista[*tamaux].Key = song.Key;
@@ -151,7 +329,7 @@ int IncRegistro(TSongs song, TSongs *lista, int *tamaux) {
 
     *tamaux = *tamaux + 1;
     return 1;
-  } else if (BuscaPorRank(song.Position, lista, *tamaux) != *tamaux) {
+  } else if (pos != *tamaux) {
     lista[*tamaux].Position = song.Position;
     lista[*tamaux].Key = song.Key;
 
@@ -181,251 +359,50 @@ int IncRegistro(TSongs song, TSongs *lista, int *tamaux) {
   return 0;
 }
 
-int RemRegistro(TSongs song, TSongs *lista, int *tamaux) {
-  int a = BuscaPorRank(song.Position, lista, *tamaux);
-  if (a != 0) {
-    // //printf("Entrou na remoçao");
-    // int i = 0;
-    // // percorro a *lista até encontrar a posição
-    // while(1)
-    // {
-    //   if(lista[i].Position == song.Position)
-    //   {
-    // limpa os campos
-    limpaRegistro(lista[a]);
+int insereNaLista(TSongs song, TListaLinear *listaLinear) {
+//printf("entrou na inserção\n");
+  int pos = buscaNaLista(song, listaLinear);
 
-    // atribui a ultima posição a que eu quero excluir
-    //   lista[i] = lista[*tamaux];
-    //   *tamaux = *tamaux - 1;
-    //   break;
-    // }
-    // i++;
-    *tamaux = *tamaux - 1;
-    return 1;
+  if (listaLinear->lista[pos].Position != song.Position) {
+//printf("entrou no if");
+    if (listaLinear->eOrd != 0){
+//printf("entrou na inserção ordenada\n");
+      return IncRegistroOrd(song, listaLinear->lista, &(listaLinear->tam), pos);
+    }else{
+//printf("entrou na inserção !ordenada");
+      return IncRegistro(song, listaLinear->lista, &(listaLinear->tam), pos);}
   }
-  return 0;
-}
-
-int busca_binaria(TSongs *lista, int tamanho, int item) {
-    int esquerda = 0, direita = tamanho - 1;
-
-    while (esquerda <= direita) {
-        int meio = (esquerda + direita) / 2;
-        if (lista[meio].Position == item) {
-            return meio;
-        } else if (lista[meio].Position < item) {
-            esquerda = meio + 1;
-        } else {
-            direita = meio - 1;
-        }
-    }
-
-    return esquerda;
-}
-
-int buscaBinporPOSICAO(int position, TSongs *lista, int tam, int *existe) {
-  //essa função sempre vai retornar um indice, o ponteiro existe
-  //que define se existe ou não.
-  // << *existe = 0 que dizer que a chave buscada nao existe, e função vai retorar onde ele deveria esta >>
-  //*existe = 1, a função vai retornar onde foi encontrado
-  int inicio = 0;                // inicio do vetor
-  int meio = (inicio + tam) / 2; // meio do vetor
-  *existe = 1; //diz se exies ou nao
-  while (inicio <= tam) {
-    if (position == lista[meio].Position) // verificação se o elemento procurado está no meio
-      return meio;
-    else {
-      if (position < lista[meio].Position) // verifica se o elemento do meio é
-                                           // maior do que o procurado
-        tam = meio - 1;
-      else // ou se é menor
-        inicio = meio + 1;
-    }
-    meio = (inicio + tam) / 2; // calculando o meio do vetor
-  }
-  *existe = 0; // elemento não encontrado
-  return inicio; // elemento não encontrado e deveria esta nessa indice
-}
-
-int buscaBinporPOSICAOi(int position, TSongs *lista, int tam) {
-  int inicio = 0;                // inicio do vetor
-  int meio = (inicio + tam) / 2; // meio do vetor
-
-  while (inicio <= tam) {
-    if (position ==
-        lista[meio]
-            .Position) // verificação se o elemento procurado está no meio
-      return meio;
-    else {
-      if (position < lista[meio].Position) // verifica se o elemento do meio é
-                                           // maior do que o procurado
-        tam = meio - 1;
-      else // ou se é menor
-        inicio = meio + 1;
-    }
-    meio = (inicio + tam) / 2; // calculando o meio do vetor
-  }
-  return -1; // elemento não encontrado
-}
-
-int IncRegistroOrd(TSongs song, TSongs *lista, int *tamaux) {
-  int pos = busca_binaria(lista, *tamaux, song.Position);
-
-  if (lista[pos].Position != song.Position) {
-    for (int i = *tamaux; i > pos; i--)  
-      lista[i+1] = lista[i];  // Abre espaço na lista para colocar o registro na posição correta
-
-    lista[pos].Position = song.Position;
-    lista[pos].Key = song.Key;
-
-    // dava erro no tamanho estranhamente entao ao *2
-    // garante que vai ter o espaço
-    lista[pos].ArtistName = (char *)malloc(strlen(song.ArtistName) * 2);  
-    strcpy(lista[pos].ArtistName, song.ArtistName);
-
-    lista[pos].SongName = (char *)malloc(strlen(song.SongName) * 2);
-    strcpy(lista[pos].SongName, song.SongName);
-
-    lista[pos].Days = song.Days;
-    lista[pos].Top10Times = song.Top10Times;
-    lista[pos].PeakPosition = song.PeakPosition;
-
-    lista[pos].PeakPositionXtimes =
-        (char *)malloc(strlen(song.PeakPositionXtimes) * 2);
-    strcpy(lista[pos].PeakPositionXtimes, song.PeakPositionXtimes);
-
-    lista[pos].PeakStreams = song.PeakStreams;
-    lista[pos].TotalStreams = song.TotalStreams;
-
-    *tamaux = *tamaux + 1;
-    return 1;
-  }
-
-  return 0;
-}
-
-int RemRegistroOrd(TSongs song, TSongs *lista, int *tamaux) {
-  int pos = busca_binaria(lista, *tamaux, song.Position);
-  
-  if (lista[pos].Position == song.Position) {    
-    // limpa os campos
-    limpaRegistro(lista[pos]);
-
-    if(*tamaux > 1)
-      for (int i = *tamaux; i > pos; i--)
-        lista[i-1] = lista[i];
-
-    *tamaux = *tamaux - 1;
-    return 1;
-  }
-
-  return 0;
-}
-
-int RemRegistro_nao_ordenado(TSongs song, TSongs *lista, int *tamaux) {
-  int a = BuscaPorRank(song.Position, lista, *tamaux);
-  if (a != *tamaux) {
-    limpaRegistro(lista[a]);
-    lista[a] = lista[*tamaux - 1];
-    *tamaux = *tamaux - 1;
-    return 1;
-  }
-  return 0;
-}
-
-int RemRegistro_ordenado(TSongs song, TSongs *lista, int *tamaux) {
-
-  int a = buscaBinporPOSICAOi(song.Position,lista, *tamaux);
-  if (a != -1) {
-   
-  
-    free(lista[a].ArtistName);
-    free(lista[a].SongName);
-    free(lista[a].PeakPositionXtimes);
-    lista[a] = lista[*tamaux -1];
-    for (int i = a + 1; i < *tamaux; i++) {
-      lista[i] = lista[i + 1];       
-    }
-    
-    *tamaux = *tamaux - 1;
-    return 1;
-  }
-  return 0;
-}
-
-TListaLinear* criaListaLinear(int cap, int eOrd) {
-  TListaLinear *listaLinear;
-  listaLinear->cap = cap;
-  listaLinear->eOrd = eOrd;
-
-  if (eOrd) 
-    listaLinear->lista = (TSongs*) malloc(cap * sizeof(TSongs));
-  else
-    listaLinear->lista = (TSongs*) malloc((cap+1) * sizeof(TSongs));
-  
-  if (!listaLinear->lista) {
-    printf("Erro de alocacao!!!");
-    exit(-1);
-  }
-
-  return listaLinear;
 }
 
 /*
-NÃO TESTEI AINDA E NÃO SEI SE (lista -> lista) ESTA CERTO
-  */
-int buscaNaLista(TSongs song, TListaLinear* listaLinear){
-  int posicao;
-
-  if(listaLinear->tam == 0){
-    return 0;
-  } else if(listaLinear->eOrd != 0){
-    posicao = busca_binaria(listaLinear->lista, listaLinear->tam, song.Position);
-    return posicao;
-  } else{
-    posicao = BuscaPorRank(song.Position, listaLinear->lista, listaLinear->tam);
-    return posicao;
-  }
-}
-
-int insereNaLista(TSongs song, TListaLinear* listaLinear) {
-  //int pos = buscaNaLista(song, listaLinear);
-  
-  if (listaLinear->eOrd != 0) 
-    return IncRegistroOrd(song, listaLinear->lista, &(listaLinear->tam));
-  else 
-    return IncRegistro(song, listaLinear->lista, &(listaLinear->tam));
-}
-
-/* 
-  As funções de remoção foram modificadas para que retornem um ponteiro para o registro removido da lista
-  e retirei as funções de busca, pois a função "buscaNaLista" que provavelmente deve ser usada
-  No momento coloquei "_" para diferenciar, mas não está no .h
+  As funções de remoção foram modificadas para que retornem um ponteiro para o
+  registro removido da lista Foram retiradas as funções de busca das funções de
+  remoção, pois a busca é feita no "insereNaLista"
 */
 
-TSongs* RemRegistroOrd_(TSongs song, TSongs *lista, int *tamaux, int pos) {
+TSongs *RemRegistroOrd(TSongs song, TSongs *lista, int *tamaux, int pos) {
   // Ponteiro para o registro que será removido da lista
   TSongs *ptr = &(lista[pos]);
-  
-  if (lista[pos].Position == song.Position) {    
+
+  if (lista[pos].Position == song.Position) {
     // limpa os campos
     limpaRegistro(lista[pos]);
 
-    if(*tamaux > 1)
+    if (*tamaux > 1)
       for (int i = *tamaux; i > pos; i--)
-        lista[i-1] = lista[i];
+        lista[i - 1] = lista[i];
 
     *tamaux = *tamaux - 1;
-
     return ptr;
   }
 
   return NULL;
 }
 
-TSongs* RemRegistro_nao_ordenado_(TSongs song, TSongs *lista, int *tamaux, int pos) {
+TSongs *RemRegistro_nao_ordenado(TSongs song, TSongs *lista, int *tamaux,
+                                 int pos) {
   TSongs *ptr = &(lista[pos]);
-  
+
   if (pos != *tamaux) {
     limpaRegistro(lista[pos]);
     lista[pos] = lista[*tamaux - 1];
@@ -435,14 +412,15 @@ TSongs* RemRegistro_nao_ordenado_(TSongs song, TSongs *lista, int *tamaux, int p
   return NULL;
 }
 
-TSongs* removeDaLista(TSongs song, TListaLinear* listaLinear) {
+TSongs *removeDaLista(TSongs song, TListaLinear *listaLinear) {
+
   int pos = buscaNaLista(song, listaLinear);
-  
+
   if (listaLinear->eOrd != 0) {
-    return RemRegistroOrd_(song, listaLinear->lista, &(listaLinear->tam), pos);
-  }
-  else {
-    return RemRegistro_nao_ordenado_(song, listaLinear->lista, &(listaLinear->tam), pos);
+
+    return RemRegistroOrd(song, listaLinear->lista, &(listaLinear->tam), pos);
+  } else {
+    return RemRegistro_nao_ordenado(song, listaLinear->lista,
+                                    &(listaLinear->tam), pos);
   }
 }
- 
