84f9fba711e48baa0e4623249ae829d62a9b4fcd
Alimva <alicia.mar.val@gmail.com>
2023-05-24 13:50:46 -0300

    correçao final

--- Equipe_1/main_arv.c
+++ Equipe_1/main_arv.c
@@ -125,48 +125,44 @@ void printArv(PNoArvBin arv, int tipo)
 PNoArvBin buscaNaArv(PNoArvBin arv, int key){
     if(arv == NULL){
         return NULL;
-    }else if(arv->info.key == key)
-    {
+    }else if(arv->info.key == key){
         return arv;
-    }else if(key < arv->info.key)
-    {
+    }else if(key < arv->info.key){
         return buscaNaArv(arv->esq, key);
-    }else
-    {
+    }else{
         return buscaNaArv(arv->dir, key);
     }
 }
 
 PNoArvBin removeNoArvBin(PNoArvBin arv, TInfo info){
   
-  if(arv == NULL){
-    return NULL;
-  }
-  
-  if(arv->info.key == info.key){
-    if(arv->dir != NULL && arv->esq != NULL){
-      PNoArvBin aux = newNoArvBin();
-      aux = arv->dir;
-      while(aux->esq != NULL){
-        aux = aux->esq;
-      }
-      arv->info = aux->info;
-      arv->dir = removeNoArvBin(arv->dir,aux->info);
-     return arv;
-    }else if(arv->dir == NULL && arv->esq == NULL){
-      return NULL;
-    }else if(arv->dir != NULL){
-      return arv->dir;
-    }else if(arv->esq != NULL){
-      return arv->esq;
+    if(arv == NULL){
+        return NULL;
     }
-  }
+  
+    if(arv->info.key == info.key){
+        if(arv->dir != NULL && arv->esq != NULL){
+            PNoArvBin aux = newNoArvBin();
+            aux = arv->dir;
+            while(aux->esq != NULL){
+            aux = aux->esq;
+            }
+            arv->info = aux->info;
+            arv->dir = removeNoArvBin(arv->dir,aux->info);
+            return arv;
+        }else if(arv->dir == NULL && arv->esq == NULL){
+            return NULL;
+        }else if(arv->dir != NULL){
+            return arv->dir;
+        }else if(arv->esq != NULL){
+            return arv->esq;
+        }
+    }
     else{
         if(info.key < arv->info.key){
             arv->esq = removeNoArvBin(arv->esq, info);
             return arv;
-        }
-        else{
+        }else{
             arv->dir = removeNoArvBin(arv->dir, info);
             return arv;
         }
